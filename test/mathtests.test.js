3426b6318630b8f13c2a591a2703a4dcf101d405
Drilon Reçica <drilonrecica.dev@gmail.com>
2022-10-27 22:39:37 +0200

    Broke tests

--- test/mathtests.test.js
+++ test/mathtests.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert'); describe('Simple Math Tests', () => {
     it('should return 2', () => {
-        assert.equal(1 + 1, 2);
+        assert.equal(1 + 1, 3);
     });
     it('should return 9', () => {
         assert.equal(3 * 3, 9);
